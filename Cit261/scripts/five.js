e69de4887d41ff7595b3fa9707645ba5504bd1bc
Alyssa Hundley <ahundley11@gmail.com>
2018-07-14 21:56:59 -0600

    changes

--- Cit261/scripts/five.js
+++ Cit261/scripts/five.js
@@ -4,36 +4,39 @@ var myName = localStorage.getItem('firstName');
 document.getElementById("setGet").innerHTML = myName;
 }
 
-function arrays(){
-    
-}
 
-function arrays2(){
-    
-}
 
-function objects(){
-    var films = [
-             {
-              'id': 1,
-              'title':'John Wick',
-              'Genre':'Action'
-             },
-             {
-              'id': 2,
-              'title':'Taken 3',
-              'Genre':'Action'
-             },
-             {
-              'id': 3,
-              'title':'The Guard',
-              'Genre':'Comedy'
-             }
-            ];
-localStorage.setItem('films', JSON.stringify(films));
-var movies = localStorage.getItem('films');
-movies = JSON.parse(movies);
-document.getElementById("objects").innerHTML = movies;
-    
-}
+var rainbow = ["Red", " Orange", " Yellow", " Green", " Blue", " Violet"];
+                     
+            var family = '{"family":[' + '{ "firstName":"Sterling" , "lastName":"Hundley" },' +
+                            '{ "firstName":"Leslie" , "lastName":"Hundley" },' +
+                            '{ "firstName":"Riley" , "lastName":"Stevenson" },' +
+                            '{ "firstName":"Collin" , "lastName":"Hundley" },' +
+                            '{ "firstName":"Alyssa" , "lastName":"Stevenson" },' +
+                            '{ "firstName":"Tori" , "lastName":"Hundley" },' +
+                            '{ "firstName":"Kaylee" , "lastName":"Hundley" }]}';       
+                    
+            // Check browser support
+            if (typeof(Storage) !== "undefined") {
+                // Store array as a string with JSON
+                localStorage.setItem("rainbow", JSON.stringify(rainbow));
+                // Retrieve
+                document.getElementById("result").innerHTML = localStorage.getItem("rainbow");
+            } else {
+                document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+            }
+            
+            // Turn string back into array
+            var retrieved = localStorage.getItem("rainbow");
+            var rainbow2 = JSON.parse(retrieved);
+
+            // Some fun with arrays
+            document.getElementById("number").innerHTML = "There are <strong>" + rainbow2.length + "</strong> colors in the rainbow</i>.";
+            document.getElementById("yellow").innerHTML = "Yellow comes after <strong> " + rainbow2[1] + "</strong> in the rainbow.";
+            document.getElementById("array").innerHTML = "<b>And converted back from a string into an array, it looks like this:</b> <br />" + rainbow2;
+        
+            // Some fun with JSON arrays
+            obj = JSON.parse(family);
+            document.getElementById("old").innerHTML = "The oldest member of my family is <strong>" + obj.family[0].firstName + " " + obj.family[0].lastName + "</strong>.";
+            document.getElementById("young").innerHTML = "The youngest member is <strong>" + obj.family[6].firstName + " " + obj.family[6].lastName + "</strong>.";
 
--- Cit261/scripts/six.js
+++ Cit261/scripts/six.js
@@ -1,10 +1,4 @@
 
-function create() {
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-}
 
 function insert() {
     var newItem = document.createElement("LI");
@@ -22,3 +16,28 @@ function remove(){
     list2.removeChild(list2.childNodes[0]);
 }
 
+
+function domTestAddP(inputText)
+			{
+				var para = document.createElement("P");
+				var node = document.createTextNode(inputText);
+				para.appendChild(node);
+
+				var element = document.getElementById("div1");
+				element.appendChild(para);
+			}
+
+function domTestAddYouTube(inputText){
+    var iframe = document.createElement("IFRAME");
+
+    iframe.style.display = 'block';
+    iframe.style.margin = '0px auto';
+    iframe.width = '854';
+    iframe.height = '480';
+    iframe.src='https://www.youtube.com/embed/' + inputText;
+    iframe.frameborder = '0';
+
+    var element = document.getElementById("videoFrame1");
+
+    element.appendChild(iframe);
+}
